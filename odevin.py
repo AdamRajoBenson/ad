8b50511d7c560f008e45b93c48f4f9a430f21370
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2019-06-07 12:05:06 +0530

    Update odevin.py

--- odevin.py
+++ odevin.py
@@ -1,15 +1,7 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
-{
-int a;
-cin>>a;
-if(a<0)
-cout<<"invalid";
-else if(a%2==1)
-cout<<"odd";
-else if(a%2==0)
-cout<<"even";
-else
-cout<<"invalid";
-}
+o=int(input())
+if(o%2==0):
+  print("Even")
+elif(o%2==1):
+  print("Odd")
+else:
+  print("Invalid")
